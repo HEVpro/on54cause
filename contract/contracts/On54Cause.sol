05d816d4008e28dc0cba4cbb2cecd90d5f02fe09
jorvixsky <jorvixsky@proton.me>
2024-11-16 06:46:05 +0700

    Removed contract console log

--- contract/contracts/On54Cause.sol
+++ contract/contracts/On54Cause.sol
@@ -4,7 +4,6 @@ pragma solidity 0.8.27;
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
-import "hardhat/console.sol";
 
 interface IChronicle {
     function read() external view returns (uint256 value);
