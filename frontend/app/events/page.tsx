7f356ceb9d427684a721aae61ac56406f9c4fc4c
jorditm <jorditorrasm@gmail.com>
2024-11-16 11:36:24 +0100

    merge main

--- frontend/app/events/page.tsx
+++ frontend/app/events/page.tsx
@@ -1,9 +1,12 @@
 import ListEvents from "@/components/ListEvents";
 import getEvents from "@/lib/events";
+import { Suspense } from "react";
 
 export default async function Page() {
-
     const events = await getEvents()
-    return <ListEvents initialData={events} />
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <ListEvents initialData={events} />
+        </Suspense>)
 
 }
